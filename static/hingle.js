dab6e4a9104e6d70ead1f13d6f8ec0285a7702bc
junliangl <1650747456@qq.com>
2022-12-12 06:54:02 +0800

    Site updated: 2022-12-12 06:53:48

--- static/hingle.js
+++ static/hingle.js
@@ -164,7 +164,7 @@ var Paul_Hingle = function (config) {
     // ! Hexo 特别功能
     //
 
-    // Hexo 百度搜索
+    // Hexo 搜索
     this.hexo_search = function () {
         var form = ks.select(".head-search"), input = ks.select(".head-search input");
 
@@ -176,7 +176,45 @@ var Paul_Hingle = function (config) {
     }
 
     this.hexo_search();
+	
 };
+
+var search = function(){
+    require(['/js/search.js'], function(){
+        var inputArea = document.querySelector("#st-search-input");
+        var $HideWhenSearch = $("#toc, #tocButton, .post-list, #post-nav-button a:nth-child(2)");
+        var $resultArea = $("#local-search-result");
+
+        var getSearchFile = function(){
+            var search_path = "/search.xml";
+            var path = search_path;
+            searchFunc(path, 'st-search-input', 'local-search-result');
+        }
+
+        var getFileOnload = inputArea.getAttribute('searchonload');
+        if (getFileOnload === "true") {
+            getSearchFile();
+        } else {
+            inputArea.onfocus = function(){ getSearchFile() }
+        }
+
+        var HideTocArea = function(){
+            $HideWhenSearch.css("visibility","hidden");
+        }
+        inputArea.oninput = function(){ HideTocArea() }
+        inputArea.onkeydown = function(){ if(event.keyCode==13) return false}
+
+        $resultArea.bind("DOMNodeRemoved DOMNodeInserted", function(e) {
+            if (!$(e.target).text()) {
+                $(".no-result").show(200);
+            } else {
+              $(".no-result").hide();
+            }
+        })
+    })
+}();
+
+
 
 // 图片缩放
 ks.image(".post-content:not(.is-special) img, .page-content:not(.is-special) img");
